384cec564189ff177d5a8597f40a8f74718be638
Friska Eka K <friska744@gmail.com>
2024-08-05 12:42:21 +0700

    tugas API

---
+++ src/test/java/stepDef/ApiStep.java
@@ -0,0 +1,8 @@
+import io.cucumber.java.en.Given;
+
+public class MyStepdefs {
+    @Given("prepare url valid for get list data")
+    public void prepareUrlValidForGetListData() {
+        System.out.println("halo step");
+    }
+}
